102ffc9b0987eef00b61e5f75cd12206ca3fda47
kuscher <alex.kuscher@gmail.com>
2026-04-16 11:18:17 +0200

    Updating assetlinks.json and re-adding android-wrapper

---
+++ android-wrapper/app/src/main/java/tools/fika/script/DelegationService.java
@@ -0,0 +1,14 @@
+package tools.fika.script;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
